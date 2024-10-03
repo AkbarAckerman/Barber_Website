eaaaff4f19b769990dd74cd48af91d7d60847715
AkbarAckerman <akbarkhon0520@gmail.com>
2024-10-03 15:39:55 +0500

    commit html

--- src/components/Barber/Barber.js
+++ src/components/Barber/Barber.js
@@ -2,20 +2,34 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './Barber.css';
+import barberImage1 from '../assets/test1.png';
+import barberImage2 from '../assets/test2.png';
+import barberImage3 from '../assets/test3.png';
+import barberImage4 from '../assets/barber_four.jpg';
+
+const barbers = [
+  { name: 'Али барбер', image: barberImage1 },
+  { name: 'Азиз барбер', image: barberImage2 },
+  { name: 'Акбар барбер', image: barberImage3 },
+  { name: 'Пепе барбер', image: barberImage4 },
+];
 
 function Barber() {
   return (
     <div className='barbers_container'>
       <Header />
       <div className='barbers_box'>
-        <div className='barber'>
-          <div className='barber_name'>Али</div>
-        </div>
-        <div className='barber'>
-          <div className='barber_name'>Азиз</div>
-        </div>
+        {barbers.map((barber, index) => (
+          <div
+            className='barber'
+            key={index}
+            style={{ backgroundImage: `url(${barber.image})` }}
+          >
+            <div className='barber_name'>{barber.name}</div>
+          </div>
+        ))}
       </div>
-      <Footer />
+
     </div>
   );
 }
