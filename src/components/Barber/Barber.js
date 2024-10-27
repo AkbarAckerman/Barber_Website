9fc2a95c41518bb8c9abad9c7a981356c59a1953
AkbarAckerman <akbarkhon0520@gmail.com>
2024-10-27 11:38:26 +0500

    update photo 3

--- src/components/Barber/Barber.js
+++ src/components/Barber/Barber.js
@@ -4,13 +4,13 @@ import Footer from '../Footer/Footer';
 import './Barber.css';
 import barberImage1 from '../assets/test1.png';
 import barberImage2 from '../assets/test2.png';
-import barberImage3 from '../assets/test3.png';
+import barberImage3 from '../assets/barber_three.webp';
 import barberImage4 from '../assets/barber_four.jpg';
 
 const barbers = [
   { name: 'Али барбер', image: barberImage1 },
   { name: 'Азиз барбер', image: barberImage2 },
-  { name: 'Акбар барбер', image: barberImage3 },
+  { name: 'Джамщид барбер', image: barberImage3 },
   { name: 'Пепе барбер', image: barberImage4 },
 ];
 
